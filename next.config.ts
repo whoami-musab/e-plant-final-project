3b759835aaf1b4ffdc22df4958b5a1e154385758
whoami-musab <musababdalmaged@gmail.com>
2025-02-24 14:53:08 +0300

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects(){
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: ["www.trimitra-perkasa.com"],
   },
